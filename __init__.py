6cc20303479509410367a77d2a16e0ecc462104d
Ingo Liebig <ilie@d-velop.de>
2024-12-20 07:56:25 +0100

    Added support for selecting mistral model

--- __init__.py
+++ __init__.py
@@ -39,6 +39,7 @@ async def setup_common(hass: HomeAssistant, conf: dict) -> bool:
         prompt = call.data.get("prompt")
         agent_id = call.data.get("agent_id")
         identifier = call.data.get("identifier")
+        model = call.data.get("model")
 
         current_state = hass.states.get("sensor.mistral_ai")
         current_attributes = current_state.attributes if current_state else {}
@@ -60,7 +61,7 @@ async def setup_common(hass: HomeAssistant, conf: dict) -> bool:
             }
         else:
             payload = {
-                "model": "mistral-large-latest",
+                "model": model,
                 "messages": [{"role": "user", "content": prompt}],
             }
 
@@ -74,7 +75,6 @@ async def setup_common(hass: HomeAssistant, conf: dict) -> bool:
             response = await asyncio.wait_for(hass.async_add_executor_job(make_request), timeout=60)
             response.raise_for_status()
             response_data = response.json()
-
             if 'choices' in response_data and 'message' in response_data['choices'][0]:
                 message_content = response_data['choices'][0]['message']['content']
                 current_state = hass.states.get("sensor.mistral_ai")
@@ -82,7 +82,8 @@ async def setup_common(hass: HomeAssistant, conf: dict) -> bool:
                 new_attributes = {**current_attributes, "timestamp": datetime.now().timestamp()}
                 hass.states.async_set("sensor.mistral_ai", "done", new_attributes)
                 
-                hass.bus.async_fire("mistral_ai_response", {"response": message_content, "identifier": identifier, "agent_id": agent_id if agent_id else ''})
+                event_data = {"response": message_content, "identifier": identifier, "agent_id": agent_id if agent_id else ''}
+                hass.bus.async_fire("mistral_ai_response", event_data)
             else:
                 _LOGGER.error(f"Unexpected response structure: {response_data}")
         except asyncio.TimeoutError:
